39527a90c24b5bf1ba7321f2bed8ba250b635740
Md Shayon <mdshayon0@gmail.com>
2019-02-07 01:24:16 +0600

    connected to database and . made mongoose schema

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+
 
 const app = express();
 
+// DB CONFIG
+const db = require('./config/keys').mongoLocalURI;
+
+
+
+// mongoose.connect(db, {
+//         useNewUrlParser: true
+//     })
+//     .then(() => console.log("Mongo db is been connected"))
+//     .catch(err => console.log("Error with mongodb " + err));
+
+
+
+mongoose.connect(db); //IF TESTAROO DB IS ALREADY EXIST THEN OK. OR IF IT ISN'T IT WILL CREATE AUTOMATICLY
+mongoose.connection.once('open', function () {
+    console.log("Connection has been made now let's make fireaowks");
+}).on('error', function (error) {
+    console.log('Connection', error);
+});
+
+
+
 
 app.use(expressLayouts); //WE NEED TO MAKE SURE THIS LAYOUT IS ABOVE VIEW ENGINE EJS SETTING
-app.set('view engine', 'ejs'); 
+app.set('view engine', 'ejs');
 
 
 
@@ -13,10 +37,10 @@ app.set('view engine', 'ejs');
 const indexRoutes = require('./routes/index');
 const usersRoutes = require('./routes/users');
 
-app.use('/', indexRoutes );
-app.use('/users', usersRoutes );
+app.use('/', indexRoutes);
+app.use('/users', usersRoutes);
 
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT , console.log(`app is running on http://localhost:${PORT}`));
+app.listen(PORT, console.log(`app is running on http://localhost:${PORT}`));
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://md_shayon:Shayon1234@cluster0-l3y9w.mongodb.net/test?retryWrites=true',
+    mongoLocalURI: 'mongodb://localhost/authsystem'
+}
