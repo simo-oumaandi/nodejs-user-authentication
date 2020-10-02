45d9c733d36c6710a52b932797fb2eb329dea266
MdSamsuzzohaShayon <mdshayon0@gmail.com>
2020-10-02 22:53:24 +0600

    basic view setup

--- index.js
+++ index.js
@@ -16,5 +16,11 @@ mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 
 
-const PORT = process.env.PORT || 6000;
+// ROUTE
+app.get('/', (req, res, next) => { res.render('home.ejs'); });
+app.get('/players', (req, res, next) => { res.render('players') });
+
+
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("Server is running on port : " + PORT))
---
+++ views/partials/footer.ejs
@@ -0,0 +1,4 @@
+</div>
+<script src="js/main.js"></script>
+</body>
+</html>
