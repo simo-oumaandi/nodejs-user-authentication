89c89bf4a3b621115bd8c9f8665ed7cdf4de56b0
Md Samsuzzoha <mdshayon0@gmail.com>
2021-01-20 16:20:57 +0600

    simple server

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+
+const app = express();
+const authRoutes = require('./routes/auth');
+
+
+app.use(express.json());
+app.use(cors());
+
+
+
+
+
+app.use('/api', authRoutes);
+
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=> console.log(`Server is running on : ${PORT}`));
