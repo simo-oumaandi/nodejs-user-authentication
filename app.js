b9b0ed1357ca696190c250f9cee132093f47383e
Md Shayon <mdshayon0@gmail.com>
2021-01-31 23:06:01 +0600

    passport.js js setup with google strategy

--- app.js
+++ app.js
@@ -2,27 +2,46 @@ const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const passport = require('passport');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
 
-
+// LOAD CONFIG 
+dotenv.config({ path: "./config/config.env" });
 
 const connectDB = require('./config/db');
 
 
+require('./config/passport')(passport);
+
+
 
 // MIDDLEWARE 
-// LOAD CONFIG 
-dotenv.config({path: "./config/config.env"});
+
 connectDB();
 const app = express();
-if(process.env.NODE_ENV === "development"){
+if (process.env.NODE_ENV === "development") {
     app.use(morgan('dev'));
 }
-app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
+app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', '.hbs');
+
+// SESSION MIDDLEWARE 
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    // cookie: { secure: true } // THIS WON'T WORK WITHOUT HTTPS
+}));
+// PASSPORT MIDDLEWARE 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', require('./routes/index'));
+app.use('/auth', require('./routes/auth'));
+
 
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log('Server is running on ' + process.env.NODE_ENV + ' mode on port: '+ PORT));
+app.listen(PORT, () => console.log('Server is running on ' + process.env.NODE_ENV + ' mode on port: ' + PORT));
---
+++ routes/auth.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const passport = require('passport');
+const router = express();
+
+
+// AUTH 
+router.get('/google', passport.authenticate('google', { scope: ['profile'] }));
+
+
+
+// CALLBACK
+router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/' }), (req, res, next) => {
+    res.redirect('/dashboard');
+});
+
+
+
+
+module.exports = router;
