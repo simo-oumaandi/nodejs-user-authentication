f5e4d722f7ca63609adf8839457e291bf19f40dc
Md Shayon <mdshayon0@gmail.com>
2021-02-02 15:56:35 +0600

    edit stories

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const morgan = require('morgan');
 const passport = require('passport');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
+const methodOverride = require('method-override');
 const exphbs = require('express-handlebars');
 
 // LOAD CONFIG 
@@ -13,17 +14,28 @@ dotenv.config({ path: "./config/config.env" });
 
 const connectDB = require('./config/db');
 
-
 require('./config/passport')(passport);
-
+const app = express();
 
 
 // MIDDLEWARE 
 
 connectDB();
-const app = express();
+
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// MOTHOD OVERRIDE
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // look in urlencoded POST bodies and delete it
+        let method = req.body._method
+        delete req.body._method
+        return method
+    }
+}));
 
 
 if (process.env.NODE_ENV === "development") {
@@ -31,10 +43,10 @@ if (process.env.NODE_ENV === "development") {
 }
 
 
-const { formatDate, stripTags, truncate, editIcon } = require('./helpers/hbs');
+const { formatDate, stripTags, truncate, editIcon, select } = require('./helpers/hbs');
 
 
-app.engine('.hbs', exphbs({ helpers: { formatDate, stripTags, truncate, editIcon }, defaultLayout: 'main', extname: '.hbs' }));
+app.engine('.hbs', exphbs({ helpers: { formatDate, stripTags, truncate, editIcon, select }, defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', '.hbs');
 
 // SESSION MIDDLEWARE 
--- routes/stories.js
+++ routes/stories.js
@@ -36,4 +36,39 @@ router.get('/', ensureAuth, async (req, res, next) => {
 
 
 
+
+
+
+
+
+// The lean option tells Mongoose to skip hydrating the result documents.
+// https://mongoosejs.com/docs/tutorials/lean.html
+router.get('/edit/:id', ensureAuth, async(req, res, next) => {
+    const story = await Story.findOne({_id: req.params.id}).lean();
+    if(!story){
+        return res.render('error/404');
+    }
+    if(story.user != req.user.id){
+        res.redirect('/stories');
+    }else{
+        res.render('stories/edit', {story});
+    }
+});
+
+
+
+router.put('/:id', ensureAuth, async (req, res, next)=>{
+    let story = await Story.findById(req.params.id).lean();
+    if(!story){
+        return res.render('error/404');
+    }
+    if(story.user != req.user.id){
+        res.redirect('/stories');
+    }else{
+        story = await Story.findOneAndUpdate({_id: req.params.id}, req.body, {new: true, runValidators: true});
+        res.redirect('/dashboard');
+    }
+});
+
+
 module.exports = router;
