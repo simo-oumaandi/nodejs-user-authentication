0a9d61812a8229fb6b607f96a4c93e83c5c613a6
Md Shayon <mdshayon0@gmail.com>
2021-02-02 16:24:06 +0600

    stories of specific user

--- routes/stories.js
+++ routes/stories.js
@@ -110,4 +110,19 @@ router.delete('/:id', ensureAuth, async (req, res, next) => {
 });
 
 
+
+
+router.get('/user/:userId', ensureAuth, async (req, res, next)=>{
+    try {
+        const stories = await Story.find({user: req.params.userId, status: 'public'}).populate('user').lean();
+        res.render('stories/index', {
+            stories
+        });
+    } catch (err) {
+        console.error(err);
+        res.render('error/500');
+    }
+});
+
+
 module.exports = router;
