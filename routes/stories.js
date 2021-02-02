58064def41c139e7b4678139937e2cbcea2a2878
Md Shayon <mdshayon0@gmail.com>
2021-02-02 16:19:01 +0600

    simple single post show request

--- routes/stories.js
+++ routes/stories.js
@@ -36,6 +36,22 @@ router.get('/', ensureAuth, async (req, res, next) => {
 
 
 
+router.get('/:id', ensureAuth, async (req, res, next) => {
+    try {
+        let story = await Story.findById(req.params.id).populate('user').lean();
+
+        if (!story) {
+            return res.render('error/404');
+        }
+        res.render('stories/show', {story});
+
+    } catch (err) {
+        console.error(err);
+        res.render('error/404');
+    }
+});
+
+
 
 
 
