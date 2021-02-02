1fa91162f55a1a13bc370a9a397e1451f2357039
Md Shayon <mdshayon0@gmail.com>
2021-02-02 16:07:30 +0600

    delete stories

--- routes/stories.js
+++ routes/stories.js
@@ -26,11 +26,11 @@ router.post('/add', ensureAuth, async (req, res, next) => {
 
 router.get('/', ensureAuth, async (req, res, next) => {
     try {
-        const stories = await Story.find({status: 'public'}).populate('user').sort({createdAt: 'desc'}).lean();
-        res.render('stories/index', {stories});
+        const stories = await Story.find({ status: 'public' }).populate('user').sort({ createdAt: 'desc' }).lean();
+        res.render('stories/index', { stories });
     } catch (err) {
         console.log(err);
-        res.render('error/500', {stories});
+        res.render('error/500', { stories });
     }
 });
 
@@ -43,30 +43,53 @@ router.get('/', ensureAuth, async (req, res, next) => {
 
 // The lean option tells Mongoose to skip hydrating the result documents.
 // https://mongoosejs.com/docs/tutorials/lean.html
-router.get('/edit/:id', ensureAuth, async(req, res, next) => {
-    const story = await Story.findOne({_id: req.params.id}).lean();
-    if(!story){
-        return res.render('error/404');
+router.get('/edit/:id', ensureAuth, async (req, res, next) => {
+    try {
+        const story = await Story.findOne({ _id: req.params.id }).lean();
+        if (!story) {
+            return res.render('error/404');
+        }
+        if (story.user != req.user.id) {
+            res.redirect('/stories');
+        } else {
+            res.render('stories/edit', { story });
+        }
+    } catch (err) {
+        console.error(err);
+        return res.render('error/500');
     }
-    if(story.user != req.user.id){
-        res.redirect('/stories');
-    }else{
-        res.render('stories/edit', {story});
-    }
+
 });
 
 
 
-router.put('/:id', ensureAuth, async (req, res, next)=>{
-    let story = await Story.findById(req.params.id).lean();
-    if(!story){
-        return res.render('error/404');
+router.put('/:id', ensureAuth, async (req, res, next) => {
+    try {
+        let story = await Story.findById(req.params.id).lean();
+        if (!story) {
+            return res.render('error/404');
+        }
+        if (story.user != req.user.id) {
+            res.redirect('/stories');
+        } else {
+            story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true, runValidators: true });
+            res.redirect('/dashboard');
+        }
+    } catch (err) {
+        console.error(err);
+        return res.render('error/500');
     }
-    if(story.user != req.user.id){
-        res.redirect('/stories');
-    }else{
-        story = await Story.findOneAndUpdate({_id: req.params.id}, req.body, {new: true, runValidators: true});
-        res.redirect('/dashboard');
+
+});
+
+
+router.delete('/:id', ensureAuth, async (req, res, next) => {
+    try {
+        await Story.remove({ _id: req.params.id });
+        res.redirect('/dashboard')
+    } catch (err) {
+        console.error(err);
+        return res.render('error/500');
     }
 });
 
