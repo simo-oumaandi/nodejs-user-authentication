7f67b4a11b7d2c5811b83ebc029def1489434572
Md Shayon <mdshayon0@gmail.com>
2021-11-09 01:38:04 +0600

    jwt auth backend

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -2,7 +2,7 @@ import { AUTH } from '../constants/actionTypes';
 
 import * as api from '../api/index.js';
 
-export const signin = (formData, navigate) = async (dispatch)=>{
+export const signin = (formData, navigate) => async (dispatch)=>{
     try {
         navigate('/')
     } catch (error) {
@@ -11,7 +11,7 @@ export const signin = (formData, navigate) = async (dispatch)=>{
 }
 
 
-export const signup = (formData, navigate) = async (dispatch)=>{
+export const signup = (formData, navigate) => async (dispatch)=>{
     try {
         navigate('/')
     } catch (error) {
---
+++ server/models/User.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    id: String,
+
+});
+export default mongoose.model('user', userSchema);
